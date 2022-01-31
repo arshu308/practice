3afc1da0bbba90a4f55cf68343cfb9bdfe769a72
unknown <arshlanrahman308@gmail.com>
2022-01-31 20:02:45 +0530

    added code

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        //1 = >100
+
+        for(int i=0; i<100;i++){
+            System.out.println("value "+i);
+        }
+    }
+}
